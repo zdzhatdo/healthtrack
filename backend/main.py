887d5c990445519721278a5d0c97bbcff161fbe9
zdzhatdo <157390414+zdzhatdo@users.noreply.github.com>
2026-06-14 16:49:13 -0400

    created database tables

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from database import engine
+import models
+
+# tell SQLalchemy to create tables defined by models
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
