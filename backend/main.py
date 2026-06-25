8c5d382f5c6545701786de765f46d0664ee184b3
zdzhatdo <157390414+zdzhatdo@users.noreply.github.com>
2026-06-25 13:28:05 -0400

    email verification

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app = FastAPI(
 # adding middleware to fix CORS blocking
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://healthtrack-liard.vercel.app", "https://healthtrack-git-main-zdzhatdos-projects.vercel.app"],
+    allow_origins=[],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
